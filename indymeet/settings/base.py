0a9ae66ad6328b69f9d4080fbe72993ea0b61fe0
Tim Schilling <schillingt@better-simple.com>
2024-01-04 13:02:07 -0600

    Fix tests with sslmode=disable, log debug for codered deploy.

--- indymeet/settings/base.py
+++ indymeet/settings/base.py
@@ -115,7 +115,7 @@ DATABASES = {
         "PASSWORD": os.environ["DB_PASSWORD"],
         "OPTIONS": {
             "client_encoding": "UTF8",
-            "sslmode": "require",
+            "sslmode": os.environ.get("DB_SSLMODE", "require"),
         },
     }
 }
